24b78e284d115586cc28649bcf529f6266f78f3f
Mario Arias <mario@marioarias.org>
2020-10-10 18:48:41 +0800

    initial gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "org.marioarias"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
